2ac0c2c31f1817a1caeac1f48e6442ac9fb5ef02
Zülal Nebin <zllnbn1997@gmail.com>
2018-12-13 00:40:11 +0300

    tex2speech

--- ttspi/Form1.Designer.vb
+++ ttspi/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,14 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.mediaPlayer = New AxWMPLib.AxWindowsMediaPlayer()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.spkbtn = New System.Windows.Forms.Button()
+        Me.savebtn = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         CType(Me.mediaPlayer, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -48,18 +50,32 @@ Partial Class Form1
         '
         'spkbtn
         '
-        Me.spkbtn.Location = New System.Drawing.Point(407, 53)
+        Me.spkbtn.Location = New System.Drawing.Point(405, 124)
         Me.spkbtn.Name = "spkbtn"
         Me.spkbtn.Size = New System.Drawing.Size(75, 23)
         Me.spkbtn.TabIndex = 2
-        Me.spkbtn.Text = "Speak"
+        Me.spkbtn.Text = "OKU"
         Me.spkbtn.UseVisualStyleBackColor = True
+        '
+        'savebtn
+        '
+        Me.savebtn.Location = New System.Drawing.Point(405, 164)
+        Me.savebtn.Name = "savebtn"
+        Me.savebtn.Size = New System.Drawing.Size(75, 23)
+        Me.savebtn.TabIndex = 3
+        Me.savebtn.Text = "KAYDET"
+        Me.savebtn.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(530, 343)
+        Me.Controls.Add(Me.savebtn)
         Me.Controls.Add(Me.spkbtn)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.mediaPlayer)
@@ -74,4 +90,6 @@ Partial Class Form1
     Friend WithEvents mediaPlayer As AxWMPLib.AxWindowsMediaPlayer
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents spkbtn As Button
+    Friend WithEvents savebtn As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
 End Class
--- ttspi/Form1.vb
+++ ttspi/Form1.vb
@@ -1,4 +1,7 @@
-﻿Public Class Form1
+﻿Imports System.Data.SqlClient
+Imports System.IO
+Public Class Form1
+    Dim bag As SqlConnection = New SqlConnection("Server=ISTANBULL;Initial Catalog=tts;Integrated Security=True")
 
     Public Const _BaseApiURL As String = "https://tts.voicetech.yandex.net/generate?text="
     Public Const _api As String = "&forma0t=0mp3&lang=tr-TR&speaker=zahar&emotion=good&key=358b6df1-6fe8-43bf-8caf-4d38d144ccd7"
@@ -7,5 +10,22 @@
         Dim deger As String = TextBox1.Text
         Dim _apiURL As String = _BaseApiURL + deger + _api
         mediaPlayer.URL = String.Format(_apiURL, System.Net.WebUtility.UrlEncode(TextBox1.Text.ToString()))
+    End Sub
+
+    Private Sub savebtn_Click(sender As Object, e As EventArgs) Handles savebtn.Click
+        'OpenFileDialog1.Title = "ses"
+        'OpenFileDialog1.Filter = "mp3 (*.mp3) | .mp3 | wav (*.wav) | .wav"
+        'If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+
+        'Dim sespath As String
+        'sespath = OpenFileDialog1.ToString()
+        'Dim fs As New FileStream(sespath, FileMode.Open, FileAccess.Read)
+        'Dim br As New BinaryReader(fs)
+        'Dim bytes As Byte() = br.ReadBytes(CInt(fs.Length))
+        'Dim cmd As New SqlCommand("insert into ses(ses) values(@ses)", bag)
+        'cmd.Parameters.AddWithValue("@ses", bytes)
+        'bag.Open()
+        'cmd.ExecuteNonQuery()
+        'bag.Close()
     End Sub
 End Class
